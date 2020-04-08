459e940fb8feb53beecccf9af0c969271ee40acd
averyboutin <rb06380@georgiasouthern.edu>
2020-04-08 17:17:17 -0400

    Added ability to load model and plot metrics

--- main.py
+++ main.py
@@ -1,64 +1,89 @@
 import os
+import time
 import numpy as np
 import pandas as pd
-from keras.models import Sequential
+import matplotlib.pyplot as plt
+from keras.models import Sequential, load_model
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # hides TensorFlow warnings
+start_time = time.time()
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # hides warnings
 
 train_data_dir = os.getcwd() + '/datasets/asl_alphabet_train/asl_alphabet_train/'
 test_data_dir = os.getcwd() + '/datasets/asl_alphabet_test/'
 
-epochs = 30
-batch_size = 29
-
 img_width, img_height = 200, 200
-input_shape = (img_width, img_height, 3)
 
-model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+if os.path.isfile('model_permanent.h5'):
+    model = load_model('model_permanent.h5')
+else:
+    epochs = 10
+    batch_size = 29
+    input_shape = (img_width, img_height, 3)
 
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
 
-model.add(Conv2D(128, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
 
-model.add(Flatten())
-model.add(Dropout(0.5))
-model.add(Dense(256, activation='relu'))
-model.add(Dense(29, activation='softmax'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
 
-model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(29, activation='softmax'))
 
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    validation_split=0.2)
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['categorical_accuracy'])
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        validation_split=0.2)
 
-train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='categorical',
-    shuffle=False,
-    subset='training')
+    train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=False,
+        subset='training')
 
-validation_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size=(img_width, img_height),
-    batch_size=batch_size,
-    class_mode='categorical',
-    shuffle=False,
-    subset='validation')
+    validation_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=False,
+        subset='validation')
+
+    STEP_SIZE_TRAIN = train_generator.n // train_generator.batch_size
+    STEP_SIZE_VALID = validation_generator.n // validation_generator.batch_size
+
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=STEP_SIZE_TRAIN,
+        epochs=epochs,
+        validation_data=validation_generator,
+        validation_steps=STEP_SIZE_VALID)
+
+    # model.save_weights('model_weights.h5')
+    model.save('model.h5')
+
+    model.evaluate_generator(validation_generator, STEP_SIZE_VALID, verbose=1)
+
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 test_generator = test_datagen.flow_from_directory(
     test_data_dir,
@@ -66,21 +91,6 @@ test_generator = test_datagen.flow_from_directory(
     batch_size=1,
     class_mode=None,
     shuffle=False)
-
-STEP_SIZE_TRAIN = train_generator.n // train_generator.batch_size
-STEP_SIZE_VALID = validation_generator.n // validation_generator.batch_size
-
-model.fit_generator(
-    train_generator,
-    steps_per_epoch=STEP_SIZE_TRAIN,
-    epochs=epochs,
-    validation_data=validation_generator,
-    validation_steps=STEP_SIZE_VALID)
-
-model.save_weights('model_weights.h5')
-model.save('model.h5')
-
-model.evaluate_generator(validation_generator, STEP_SIZE_VALID)
 
 STEP_SIZE_TEST = test_generator.n // test_generator.batch_size
 test_generator.reset()
@@ -91,13 +101,30 @@ prediction = model.predict_generator(test_generator,
 
 predicted_class_indices = np.argmax(prediction, axis=1)
 
-labels = train_generator.class_indices
-labels = dict((v, k) for k, v in labels.items())
+labels = [f.name for f in os.scandir(train_data_dir) if f.is_dir()]
+labels = sorted(labels)
 predictions = [labels[k] for k in predicted_class_indices]
 
 filenames = test_generator.filenames
-results = pd.DataFrame({"Filename": filenames,
-                        "Predictions": predictions})
-results.to_csv("results.csv", index=False)
+results = pd.DataFrame({"Filename": filenames, "Predictions": predictions})
+results.to_csv('results.csv', index=False)
 
-print('Done.')
+plt.plot(history.history['categorical_accuracy'])
+plt.plot(history.history['val_categorical_accuracy'])
+plt.title('Model Accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+hours, rem = divmod(time.time() - start_time, 3600)
+minutes, seconds = divmod(rem, 60)
+print("Execution Time:", "{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
